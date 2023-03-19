03dbbd0ab6218cb597783a2c8603b36532990fac
Anhnhi Tran <anhtran811@gmail.com>
2023-03-19 15:17:10 -0600

    Completed authentication section

---
+++ test/graphql/mutations/sign_in_user_test.rb
@@ -0,0 +1,40 @@
+require 'test_helper'
+
+class Mutations::SignInUserTest < ActiveSupport::TestCase
+  def perform(args = {})
+    Mutations::SignInUser.new(object: nil, field: nil, context: { session: {} }).resolve(**args)
+  end
+
+  def create_user
+    User.create!(
+      name: 'Test User',
+      email: 'email@example.com',
+      password: '[ommitted]',
+    )
+  end
+
+  test 'success' do
+    user = create_user
+    
+    result = perform(
+      credentials: {
+        email: user.email,
+        password: user.password
+      }
+    )
+  end
+
+  test 'failure because no credentials' do
+    assert_nil perform
+  end
+
+  test 'failure because wrong email' do
+    create_user
+    assert_nil perform(credentials: { email: 'wrong' })
+  end
+
+  test 'failure because wrong password' do 
+    user = create_user
+    assert_nil perform(credentials: { email: user.email, password: 'wrong' })
+  end
+end
